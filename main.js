b5dd766a2e5f60eaef42fafe330751557f82a4ea
forest079 <bluegx08@gmail.com>
2023-08-11 17:45:51 +0800

    初始更新

---
+++ main.js
@@ -0,0 +1,9 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import $ from 'jquery';
+// window.jQuery = window.$ = $;
+console.log($);
+import './assets/js/all.js';
+
+console.log("Hello world!");
---
+++ vite.config.js
@@ -0,0 +1,61 @@
+import { defineConfig } from 'vite';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { glob } from 'glob';
+
+import inject from '@rollup/plugin-inject';
+
+import liveReload from 'vite-plugin-live-reload';
+
+function moveOutputPlugin() {
+  return {
+    name: 'move-output',
+    enforce: 'post',
+    apply: 'build',
+    async generateBundle(options, bundle) {
+      for (const fileName in bundle) {
+        if (fileName.startsWith('pages/')) {
+          const newFileName = fileName.slice('pages/'.length);
+          bundle[fileName].fileName = newFileName;
+        }
+      }
+    },
+  };
+}
+
+export default defineConfig({
+  // base 的寫法：
+  // base: '/Repository 的名稱/'
+  base: '/web_slicing_2023_week5/',
+  plugins: [
+    liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
+    ViteEjsPlugin(),
+    moveOutputPlugin(),
+    inject({
+      $: 'jquery', // 载入 node_modules 中的 jquery
+      jQuery: 'jquery',
+      'windows.jQuery': 'jquery',
+    }),
+  ],
+  server: {
+    // 啟動 server 時預設開啟的頁面
+    open: 'pages/index.html',
+  },
+  build: {
+    rollupOptions: {
+      input: Object.fromEntries(
+        glob
+          .sync('pages/**/*.html')
+          .map((file) => [
+            path.relative(
+              'pages',
+              file.slice(0, file.length - path.extname(file).length)
+            ),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+    },
+    outDir: 'dist',
+  },
+});
